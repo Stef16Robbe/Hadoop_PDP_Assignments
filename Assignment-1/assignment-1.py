10de44b40368685adbebd9a048e11e3f75d60f40
Stef <stef.robbe@gmail.com>
2021-07-18 15:10:42 +0200

    finished 1

--- Assignment-1/assignment-1.py
+++ Assignment-1/assignment-1.py
@@ -8,16 +8,20 @@ class Ratings(MRJob):
             MRStep(reducer=self.reducer_sort_movies_by_ratings)
         ]
 
+    # get all movie ids
     def mapper_get_movies(self, _, line):
         (_, movieID, _, _) = line.split('\t')
         yield movieID, 1
     
+    # combine the count of ratings with their movie ids
     def combiner_count_ratings(self, movie_id, ratings):
         yield movie_id, sum(ratings)
     
+    # sum the count of the ratings
     def reducer_sum_rating_counts(self, movie_id, ratings):
         yield None, (sum(ratings), movie_id)
-    
+
+    # sort the movies based on their ratings    
     def reducer_sort_movies_by_ratings(self, _, movies):
         for count, movie_id in sorted(movies):
             yield (int(movie_id), int(count))
